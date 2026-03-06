e77edfc14571ce9cb0f5b4e321984bbd5f4f2014
Henry <91296491+Henry303@users.noreply.github.com>
2026-03-06 15:41:08 -0800

    Speaker information page

---
+++ website/pages/speakers.tsx
@@ -0,0 +1,48 @@
+import styles from "@/styles/Speakers.module.css"
+import Meta from "@/components/meta"
+
+function SpeakerInfo( {name, bio, image_src}: {name: string, bio: string, image_src?: string} ) {
+    let speaker_bio;
+    if (image_src) {
+      speaker_bio = (
+        <div className={styles.speaker_bio_photo}>
+          <img src={image_src} />
+          <p>{bio}</p>
+        </div>
+      );
+    } else {
+      speaker_bio = (
+        <div className={styles.speaker_bio_text}>
+          <p>{bio}</p>
+        </div>
+      )
+    }
+
+    return (
+        <>
+          <hr></hr>
+          <div className={styles.speaker_title}>
+            <h2>{name}</h2>
+          </div>
+          {speaker_bio}
+        </>
+    );
+}
+
+export default function Speakers() {
+    return (
+        <div className = {styles.entire_page}>
+          <div className={styles.title}>
+            <Meta title="Speakers" />
+            <h1>Honoring Speakers</h1>
+            <p>blurb blurb blurb</p>
+          </div>
+          <div className={styles.speaker_list}>
+            <SpeakerInfo name="Person" bio="blurb blurb blurb" />
+            <SpeakerInfo name="Albert Thomas" 
+                         bio="blurb blurb blurb" 
+                         image_src="https://linguistics.berkeley.edu/~sepomo/clearlake.jpg" />
+          </div>
+        </div>
+    );
+}
